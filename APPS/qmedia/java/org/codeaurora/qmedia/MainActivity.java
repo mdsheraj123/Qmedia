baa0cefd652f87e8c9fc6219a8047fd406f42916
Mohammad Sheraj <quic_msheraj@quicinc.com>
2022-01-23 20:03:14 +0530

    qmedia: Added immersive mode
    - Made the app fullscreen.
    - Made the navigation bar translucent.
    - Hid the status bar.
    - Start button will be always visible.
    - Update apk version to 4.1.0.
    
    Change-Id: Ideb8f2f8efbe853ad230313d093dd4d095668858

--- APPS/qmedia/java/org/codeaurora/qmedia/MainActivity.java
+++ APPS/qmedia/java/org/codeaurora/qmedia/MainActivity.java
@@ -63,6 +63,7 @@
 package org.codeaurora.qmedia;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 
@@ -81,6 +82,12 @@ import java.util.Objects;
 public class MainActivity extends AppCompatActivity {
 
     FrameLayout mFrameLayout;
+    int FLAGS_FULLSCREEN = (View.SYSTEM_UI_FLAG_LOW_PROFILE |
+            View.SYSTEM_UI_FLAG_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+    private long IMMERSIVE_FLAG_TIMEOUT = 500L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,6 +122,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        // Before setting full screen flags, we must wait a bit to let UI settle; otherwise, we may
+        // be trying to set app to immersive mode before it's ready and the flags do not stick
+        mFrameLayout.postDelayed(() -> mFrameLayout.setSystemUiVisibility(FLAGS_FULLSCREEN),
+                IMMERSIVE_FLAG_TIMEOUT);
     }
 
     private void launchFragment() {
